65949a63bb8b7e3f84cd8abd5f41ad3f485d9b89
natalia.mesrop <natalia.mesrop@softtek.com>
2024-04-09 16:30:47 +0200

    Ejercicio final

--- src/main/java/com/softtek/modelo/ProbarProveedor.java
+++ src/main/java/com/softtek/modelo/ProbarProveedor.java
@@ -1,48 +1,46 @@
-package com.softtek.modelo;
-
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.function.Supplier;
 
 public class ProbarProveedor {
-        public static int numeroAleatorio(int min, int max) {
-            Random random = new Random();
-            return random.nextInt(max - min + 1) + min;
+    public static Supplier<Integer> numeroAleatorio = () -> {
+        Random random = new Random();
+        int min = 1, max = 10;
+        return random.nextInt(max - min + 1) + min;
+    };
+
+    public static Supplier<List<String>> listaPredefinida = () -> {
+        return Arrays.asList("silla", "mesa", "ordenador");
+    };
+
+    public static Supplier<LocalDateTime> fechaHoraActual = () -> {
+        return LocalDateTime.now();
+    };
+
+    public static Supplier<String> obtenerStringVacio = () -> {
+        return new String();
+    };
+
+    public static Supplier<Configuracion> obtenerConfiguracion = () -> {
+        return new Configuracion("IntelliJ/configuracion");
+    };
+
+    static class Configuracion {
+        String rutaArchivo;
+
+        public Configuracion(String rutaArchivo) {
+            this.rutaArchivo = rutaArchivo;
         }
+    }
 
-        public static List<String> listaPredefinida() {
-            return Arrays.asList("silla", "mesa", "ordenador");
-        }
-
-        public static LocalDateTime fechaHoraActual() {
-            return LocalDateTime.now();
-        }
-
-        public static String obtenerStringVacio() {
-            return new String();
-        }
-
-        public static Configuracion obtenerConfiguracion() {
-            return new Configuracion("IntelliJ/configuracion");
-        }
-
-        static class Configuracion {
-            String rutaArchivo;
-
-            public Configuracion(String rutaArchivo) {
-                this.rutaArchivo = rutaArchivo;
-            }
-
-        }
-
-        public static void main(String[] args) {
-            System.out.println("Número aleatorio entre 1 y 10: " + numeroAleatorio(1, 10));
-            System.out.println("Lista predefinida: " + listaPredefinida());
-            System.out.println("Fecha y hora actual: " + fechaHoraActual());
-            System.out.println("objeto String vacío: '" + obtenerStringVacio() + "'");
-            Configuracion config = obtenerConfiguracion();
-            System.out.println("ruta " + config.rutaArchivo);
-
+    public static void main(String[] args) {
+        System.out.println("Número aleatorio entre 1 y 10: " + numeroAleatorio.get());
+        System.out.println("Lista predefinida: " + listaPredefinida.get());
+        System.out.println("Fecha y hora actual: " + fechaHoraActual.get());
+        System.out.println("objeto String vacío: '" + obtenerStringVacio.get() + "'");
+        Configuracion config = obtenerConfiguracion.get();
+        System.out.println("ruta " + config.rutaArchivo);
     }
 }
